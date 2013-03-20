c91998f7a09e60317039189cede6f22eb5ccf108
matao <ma.tao@agree.com.cn>
2013-03-20 05:05:21 +0000

    delete junk file

--- lib/configure.lua
+++
@@ -1,9 +0,0 @@
---[[
-    configure
---]]
-module(...,package.seeall)
-
-local os=require("os")
-
-PATH="./?.lua;"..os.getenv("HOME").."/lib/lua/?.lua"
-CPATH="./?.so;"..os.getenv("HOME").."/lib/lua/?.so"
