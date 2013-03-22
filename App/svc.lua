1ab11735a894f2b1b7a29f1a7efcab937d0fb2ce
yangquanxin <yang.qx@agree.com.cn>
2013-03-22 02:55:17 +0000

    添加数据库支持

--- App/svc.lua
+++ App/svc.lua
@@ -2,10 +2,7 @@ local os = require "os"
 package.path = package.path .. ";" .. os.getenv("HOME") .. "/lib/lua/?.lua"
 package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/lib/lua/?.so"
 
-local configure=require("configure")
-package.path=configure.svc.PATH
-package.cpath=configure.CPATH
-
+local configure=require("Configure")
 local node = require "node"
 local pprint = require "pprint"
 local cjson = require "cjson"
@@ -18,7 +15,9 @@ local ffi = require "ffi"
 
 local RID = "@router"
 local MQID = "@mq"
+local MTRCFG = "@conf"
 
+local confpath = "/Configure/_STRUCTURE/MonitorList[00]/Monitor/"
 
 function main()
    local _router,_amq
@@ -35,8 +34,14 @@ function main()
    local processes = {}
    local err
 
-   for i=0,configure.svc.SVC_MAX do
-      err,processes[i]= glr.spawn(configure.LUA_HOME .. "svc.lua","worker")
+   local conf = configure.new(os.getenv("HOME") .. "/cfg/shmfile/node.conf","r")
+   glr.global(MTRCFG,conf)
+   local err,svcmax = pcall(conf.get,conf,confpath .. "Base/SVCLimits")
+   if not err then
+       error(svcmax)
+   end
+   for i=0,tonumber(svcmax) do
+      err,processes[i]= glr.spawn(os.getenv("HOME") .. "/lib/lua/" .. "svc.lua","worker")
    end
 
     while true do
@@ -57,19 +62,28 @@ end
 function worker()
    local _router = glr.get_global(RID)
    local _amq = glr.get_global(MQID)
-   
+
    local nq = _amq:NQArray():get(0)
-   
+
    -- sqlite access connection create
-   
-   local dbtype = configure.svc.db.DB_Type
-   local sqlite_conn
-   if dbtype == "SQLite3" then
-      local sqlite_path = configure.svc.db.Connection.Path
-      sqlite_conn = dbaccess.connect(dbtype,sqlite_path)
-      --local sqlite_env = dbaccess.AC_Environment:new()
-      --local sqlite_conn = sqlite_env:AC_Connect("SQLite3",sqlite_path)
+
+   local conf = glr.get_global(MTRCFG)
+   local err,dbtype = pcall(conf.get,conf,confpath .. "SVC/DBConnection/_SCROLLTYPE")
+   if not err then
+       error(dbtype)
    end
+   local sqlite_conn
+   if dbtype == "4" then
+      local err,sqlite_path = pcall(conf.get,conf,confpath .. "SVC/DBConnection/_UNIONSELECTOR/SQLITE/DBFile")
+      if not err then
+          error(sqlite_path)
+      end
+      err,sqlite_conn = pcall(dbaccess.connect,"SQLite3",sqlite_path)
+      if not err then
+          error(sqlite_conn)
+      end
+   end
+
    while true do
       local msg = nq:get()
       print("svc:::",#msg, msg)
@@ -78,9 +92,9 @@ function worker()
       local content = string.sub(msg, ffi.sizeof(request_header)+1)
       pprint.pprint(#msg)
       local request_body = cjson.decode(content)
-      
 
-      -- local item=_router:find_by_name(request.from.name) 
+
+      -- local item=_router:find_by_name(request.from.name)
       -- print("router:::",pprint.format(item))
       pprint.pprint( request_header.Head.Action, "Action::")
       if request_header.Head.Action == ffi.C.ACT_REPORT then
@@ -91,7 +105,7 @@ function worker()
             local trade = require(route_info.app_type)
             local app = trade.Report:new()
             local logname =string.format("%s.log",route_info.app_type)
-            local _logger = logger.new(logname)	   
+            local _logger = logger.new(logname)
             local __gala__ = {route_info=route_info, _router=_router,_logger=_logger,_db=sqlite_conn, _header = request_header}
             app:init(__gala__, request_body)
             app:Run()
@@ -107,7 +121,7 @@ function worker()
          local app = trade.Request:new()
          local logname =string.format("%s.log",route_info.app_type)
          local logger = logger.new(logname)
-         local sqlite_path = configure.svc.db.sqlite
+         --local sqlite_path = configure.svc.db.sqlite
          -- local sqlite_env = dbaccess.AC_Environment:new()
          -- local sqlite_conn = sqlite_env:AC_Connect("SQLite3",sqlite_path)
          local __gala__ = {route_info=route_info,logger=logger,db_connection=sqlite_conn, _router=_router,  _header = request_header}
@@ -115,7 +129,7 @@ function worker()
          app:Run()
          logger:finalizer()
       end
-      
+
    end
    -- close db connect
    sqlite_conn:close()
