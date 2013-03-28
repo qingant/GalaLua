7c35383ab202638b196a4c2572f3bb6761ac509e
yangquanxin <yang.qx@agree.com.cn>
2013-03-28 03:53:38 +0000

    修改svc

--- App/svc.lua
+++ App/svc.lua
@@ -6,9 +6,9 @@ local os = require "os"
 package.path = package.path .. ";" .. os.getenv("HOME") .. "/lib/lua/?.lua"
 package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/lib/lua/?.so"
 
-local configure=require("configure")
-package.path=configure.svc.PATH
-package.cpath=configure.CPATH
+local configure=require("Configure")
+--package.path=configure.svc.PATH
+--package.cpath=configure.CPATH
 
 local node = require "node"
 local pprint = require "pprint"
@@ -19,10 +19,16 @@ local logger = require "logger"
 local dbaccess = require "dbaccess"
 local structs = require "structs"
 local ffi = require "ffi"
+local collections = require("collections")
 
 local RID = "@router"
 local MQID = "@mq"
+local MTCFGID = "@conf"
 
+local confdir = os.getenv("HOME") .. "/cfg/shmfile"
+local conffile = "/node.conf"
+local basepath = "/Configure/_STRUCTURE/MonitorList[00]/Monitor/"
+local mode = "w"
 
 function main()
    --node.register("0.0.0.0", 2345)
@@ -36,14 +42,13 @@ function main()
    init_fake_global_var()
 
    local err,status_server = glr.spawn("status", "main")
-   print("Status :::::" .. status_server)
+   print("Status :::::" .. status_server) 
 
-   local processes = {}
-   local err
+   local conf = configure.new(confdir .. conffile,mode)
+   glr.global(MTCFGID,conf)
 
-   for i=0,configure.svc.SVC_MAX do
-      err,processes[i]= glr.spawn("svc","worker")
-   end
+   local err,dispatcher = glr.spawn("svc","dispatcher")
+   print("svc dispatcher :::::" .. dispatcher)
 
    local msg_table = ffi.new("BIND_MSG")
    while true do
@@ -62,23 +67,65 @@ function main()
    end
 end
 
-function worker()
+function dispatcher()
+   local _amq = glr.get_global(MQID)
+   local nq = _amq:NQArray():get(0)
+
+   local workers = collections.List.new()
+   for i=1,100 do
+      local err, worker = glr.spawn("svc", "worker", __id__)
+      collections.List.pushright(workers, worker)
+   end
+   function collect_idle()
+      while glr.msg_available() do
+         local msg_type, addr, msg = glr.recv()
+         if msg_type ~= glr.CLOSED then
+            local p = cjson.decode(msg)
+            collections.List.pushright(workers, p.gpid)
+         end
+      end
+   end
+   while true do
+      local msg = nq:get()
+
+      if collections.List.empty(workers) then
+         collect_idle()
+      end
+      if collections.List.empty(workers) then
+         local err, worker = glr.spawn("svc", "worker", __id__)
+         collections.List.pushright(workers, worker)
+      end
+      local worker = collections.List.popleft(workers)
+      glr.send(worker, msg)
+   end
+end
+
+function worker(dispatcherid)
 --   local _router = glr.get_global(RID)
     local _router=router.router()
    local _amq = glr.get_global(MQID)
-   
+
    local nq = _amq:NQArray():get(0)
-   
+
    -- sqlite access connection create
-   
-   local dbtype = configure.svc.db.DB_Type
-   local sqlite_conn
-   if dbtype == "SQLite3" then
-      local sqlite_path = configure.svc.db.Connection.Path
-      sqlite_conn = dbaccess.connect(dbtype,sqlite_path)
-      --local sqlite_env = dbaccess.AC_Environment:new()
-      --local sqlite_conn = sqlite_env:AC_Connect("SQLite3",sqlite_path)
+
+   local conf = glr.get_global(MTCFGID)
+   local err,dbtype = pcall(conf.get,conf,basepath .. "SVC/DBConnection/_SCROLLTYPE")
+   if not err then
+       error(dbtype)
    end
+   local sqlite_conn
+   if dbtype == "4" then
+      local err,sqlite_path = pcall(conf.get,conf,basepath .. "SVC/DBConnection/_UNIONSELECTOR/SQLITE/DBFile")
+      if not err then
+          error(sqlite_path)
+      end
+      err,sqlite_conn = pcall(dbaccess.connect,"SQLite3",sqlite_path)
+      if not err then
+          error(sqlite_conn)
+      end
+   end
+
    while true do
       local msg = nq:get()
       print("svc:::",#msg, msg)
@@ -87,25 +134,25 @@ function worker()
       local content = string.sub(msg, ffi.sizeof(request_header)+1)
       pprint.pprint(#msg)
       local request_body = cjson.decode(content)
-      
 
-      -- local item=_router:find_by_name(request.from.name) 
+
+      -- local item=_router:find_by_name(request.from.name)
       -- print("router:::",pprint.format(item))
       pprint.pprint( request_header.Head.Action, "Action::")
       if request_header.Head.Action == ffi.C.ACT_REPORT then
-         
+
          -- print("router:::",pprint.format(route_info))
          pprint.pprint(_router:find_by_field("display"))
          local app_type = structs.str_pack(request_header.To.AppType)
          local trade = require(app_type)
          local app = trade.Report:new()
          local logname =string.format("%s.log",app_type)
-         local _logger = logger.new(logname)	   
+         local _logger = logger.new(logname)
          local __gala__ = {_router=_router,_logger=_logger,_db=sqlite_conn, _header = request_header}
          app:init(__gala__, request_body)
          app:Run()
          _logger:finalizer()
-         
+
       elseif request_header.Head.Action == ffi.C.ACT_REQUEST then
          local route_info = _router:find_by_name(structs.str_pack(request_header.From.Name))
          print("router:::",pprint.format(route_info))
@@ -122,7 +169,7 @@ function worker()
          app:Run()
          logger:finalizer()
       end
-      
+      glr.send(dispatcherid, cjson.encode({gpid = __id__}))
    end
    -- close db connect
    sqlite_conn:close()
