9ddc399b5f074d2edbf3e9c14eb8d55b125de921
luozhihui <luo.zh@agree.com.cn>
2013-04-10 10:03:04 +0000

    minizip 代码, 待完善的example

--- lib/lua-zip/example.lua
+++ lib/lua-zip/example.lua
@@ -3,56 +3,33 @@
 -- camel.flying@gmail.com
 
 local zip = require("minizip")
+require("os")
 
-local test_zip_file = "lzh.zip"
+local zf_name="lzh.zip"
 
-function test_open_close()
-   local archive = assert(zip.open("./test.zip", zip.CREATE))
-   archive:close()
-
-   local err = select(2, zip.open("test_opencreate_close.zip"))
-
-   --archive = assert(zip.open(test_zip_file, zip.CREATE))
-   --archive:close()
-
-   do
-      local ar = assert(zip.open("test_open_close.zip",
-                                 zip.OR(zip.CREATE,
-                                        zip.EXCL,
-                                        zip.CHECKCONS)))
-      -- 测试异常情况下gc回收ar对象。
-      --ar:close()
-   end
-   print("we need gc here")
-   collectgarbage"collect"
-end
-
-function test_file_count()
-   local zf = assert(zip.open("test.zip"))
-
-   --for file in zf:get_num_files() do
-   --   print(file.name)
-   --end
-   print("test.zip files number = " .. zf:get_num_files())
-end
-
-function test_get_name()
-    local zf = assert(zip.open("test.zip"))
-    local name = nil
-    -- 获取zip文件中的文件列表
-    for index=zf:get_num_files() , 1, -1 do
-       name = zf:get_name(1, zip.FL_UNCHANGED)
-       print(name)
-    end
-
-    zf:close()
-end
-
--- 测试主函数
+-- 主函数
 function main()
-   test_open_close()
-   test_file_count()
-   test_get_name()
+   os.execute("rm -f " .. zf_name)
+
+   -- 创建一个zip文件
+   local zf = assert(zip.open(zf_name, zip.CREATE))
+
+   print("example go ...")
+
+   -- 增加文件到zip文件中
+   --zf.add("test_minizip.lua", "file", _0, 0, -1)
+   zf:add("dir/add.txt", "string", "Contents")
+   zf:close()
+
+   zf = assert(zip.open(zf_name, zip.CHECKCONS))
+   -- 操作文件对象
+  -- local file = zf.open("add.TXT", zip.OR(zip.FL_NOCASE, zip.FL_NODIR))
+   local ctx = assert(file:read(256))
+   print(ctx)
+   file:close()
+
+   -- 关闭一个代开的zip文件
+   zf:close()
 end
 
 -- 测试入口
