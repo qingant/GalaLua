26b3e92063a7afb8d98a6c59592ff27439951d71
yangquanxin <yang.qx@agree.com.cn>
2013-04-11 10:03:30 +0000

    delete 旧文件

--- App/svc.lua
+++ App/svc.lua
@@ -34,18 +34,15 @@ local basepath = "/Configure/_STRUCTURE/MonitorList[00]/Monitor/"
 local mode = "w"
 
 function main()
-   --node.register("0.0.0.0", 2345)
    local _router,_amq
    function init_fake_global_var()
---      _router=router.new("router")
---      glr.global(RID,_router, "Router4Lua")
       _amq = amq.new(os.getenv("HOME") .. "/channels/testAMQ.chl")
       glr.global(MQID, _amq, "CGalaxyMQ");
    end
    init_fake_global_var()
 
    local err,status_server = glr.spawn("status", "main")
-   print("Status :::::" .. status_server) 
+   print("Status :::::" .. status_server)
 
    local conf = configure.new(confdir .. conffile,mode)
    glr.global(MTCFGID,conf)
@@ -65,7 +62,7 @@ function main()
             msg_table.Head.Action = ffi.C.ACT_ACK
             if not glr.send(addr, structs.pack(msg_table)) then
                 pprint.pprint("Send Failure")
-            end 
+            end
         end
    end
 end
@@ -73,11 +70,18 @@ end
 function dispatcher()
    local _amq = glr.get_global(MQID)
    local nq = _amq:NQArray():get(0)
-   
+
    local workers = collections.List.new()
-   for i=1,10 do
+   local conf = glr.get_global(MTCFGID)
+   local err,threads = pcall(conf.get,conf,basepath .. "SVC/Threads")
+   if not err then
+       error(threads)
+   end
+   local thread_total = 0
+   for i=1,tonumber(threads) do
       local err, worker = glr.spawn("svc", "worker", __id__)
       collections.List.pushright(workers, worker)
+      thread_total = thread_total + 1
    end
    function collect_idle()
       while glr.msg_available() do
@@ -97,13 +101,98 @@ function dispatcher()
       if collections.List.empty(workers) then
          collect_idle()
       end
+      while(thread_total == config.svc.THREAD_MAX) and
+        collections.List.empty(workers) do
+         local msg_type, addr, msg = glr.recv()
+         if msg_type ~= glr.CLOSED then
+            local p = cjson.decode(msg)
+            collections.List.pushright(workers, p.gpid)
+         end
+      end
       if collections.List.empty(workers) then
          local err, worker = glr.spawn("svc", "worker", __id__)
          collections.List.pushright(workers, worker)
+         thread_total = thread_total + 1
       end
       local worker = collections.List.popleft(workers)
       glr.send(worker, msg)
    end
+end
+
+function db_conn(config)
+   local err,dbtype = pcall(config.get,config,basepath .. "SVC/DBConnection/_SCROLLTYPE")
+   if not err then
+       error(dbtype)
+   end
+   local dbconn
+   if dbtype == "4" then
+      local err,sqlite_path = pcall(config.get,config,basepath .. "SVC/DBConnection/_UNIONSELECTOR/SQLITE/DBFile")
+      if not err then
+          error(sqlite_path)
+      end
+      err,dbconn = pcall(dbaccess.connect,"SQLite3",sqlite_path)
+      if not err then
+          error(dbconn)
+      end
+   elseif dbtype == "1" then
+      local err,dbname = pcall(config.get,config,basepath .. "SVC/DBConnection/_UNIONSELECTOR/ORACLE/Name")
+      if not err then
+         error(dbname)
+      end
+      local err,usr = pcall(config.get,config,basepath .. "SVC/DBConnection/_UNIONSELECTOR/ORACLE/User")
+      if not err then
+         error(usr)
+      end
+      local err,passwd = pcall(config.get,config,basepath .. "SVC/DBConnection/_UNIONSELECTOR/ORACLE/Passwd")
+      if not err then
+         error(passwd)
+      end
+      err,dbconn = pcall(dbaccess.connect,"Oracle",dbname,usr,passwd)
+      if not err then
+          error(dbconn)
+      end
+   elseif dbtype == "2" then
+      local err,dbname = pcall(config.get,config,basepath .. "SVC/DBConnection/_UNIONSELECTOR/DB2/Name")
+      if not err then
+         error(dbname)
+      end
+      local err,usr = pcall(config.get,config,basepath .. "SVC/DBConnection/_UNIONSELECTOR/DB2/User")
+      if not err then
+         error(usr)
+      end
+      local err,passwd = pcall(config.get,config,basepath .. "SVC/DBConnection/_UNIONSELECTOR/DB2/Passwd")
+      if not err then
+         error(passwd)
+      end
+      err,dbconn = pcall(dbaccess.connect,"DB2",dbname,usr,passwd)
+      if not err then
+          error(dbconn)
+      end
+   end
+   return dbconn
+end
+
+function request_handle(header)
+   local app_type = structs.str_pack(header.To.AppType)
+   local trade,app
+   if header.Head.Action == ffi.C.ACT_REPORT then
+      trade = require(app_type .. ".server.report")
+      print(app_type .. ".server.report")
+      app = trade.Report:new()
+   elseif header.Head.Action == ffi.C.ACT_REQUEST then
+      trade = require(app_type .. ".server.request")
+      print(app_type .. ".server.request")
+      app = trade.Request:new()
+   elseif header.Head.Action == ffi.C.ACT_SUBSCRIB then
+      trade = require(app_type .. ".server.subscrib")
+      print(app_type .. ".server.subscrib")
+      app = trade.Subscrib:new()
+   elseif header.Head.Action == ffi.C.ACT_CONTROL then
+      trade = require(app_type .. ".server.control")
+      print(app_type .. ".server.control")
+      app = trade.Control:new()
+   end
+   return app
 end
 
 function worker(dispatcherid)
@@ -113,71 +202,30 @@ function worker(dispatcherid)
 
    local nq = _amq:NQArray():get(1)
 
-   -- sqlite access connection create
-
    local conf = glr.get_global(MTCFGID)
-   local err,dbtype = pcall(conf.get,conf,basepath .. "SVC/DBConnection/_SCROLLTYPE")
-   if not err then
-       error(dbtype)
-   end
-   local sqlite_conn
-   if dbtype == "4" then
-      local err,sqlite_path = pcall(conf.get,conf,basepath .. "SVC/DBConnection/_UNIONSELECTOR/SQLITE/DBFile")
-      if not err then
-          error(sqlite_path)
-      end
-      err,sqlite_conn = pcall(dbaccess.connect,"SQLite3",sqlite_path)
-      if not err then
-          error(sqlite_conn)
-      end
-   end
+   local dbconn = db_conn(conf)
+
    package.path=config.svc.PATH
 
    while true do
       local msg_type, addr, msg = glr.recv()
-      -- print("svc:::",#msg, msg)
       local request_header = ffi.new("APP_HEADER")
       ffi.copy(request_header, msg, ffi.sizeof(request_header))
       local content = string.sub(msg, ffi.sizeof(request_header)+1)
-      -- pprint.pprint(#msg)
       local request_body = cjson.decode(content)
       local item = _router:find_by_name(structs.str_pack(request_header.From.Name))
-      print("router:::",pprint.format(item))
       pprint.pprint( request_header.Head.Action, "Action::")
-      if false then
-         print("over", __id__)
-      elseif request_header.Head.Action == ffi.C.ACT_REPORT then
 
-         -- print("router:::",pprint.format(route_info))
-         -- pprint.pprint(_router:find_by_field("display"))
-         local app_type = structs.str_pack(request_header.To.AppType)
-         local trade = require(app_type .. ".server.report")
-         print(app_type .. ".server.report")
-         local app = trade.Report:new()
-         local logname =string.format("%s.log",app_type)
-         local _logger = logger.new(logname)
-         local __gala__ = {_router=_router,_logger=_logger,_db=sqlite_conn, _header = request_header, _router_info = item}
-         app:init(__gala__, request_body)
-         app:Run()
-         _logger:finalizer()
-
-      elseif request_header.Head.Action == ffi.C.ACT_REQUEST then
-         local route_info = _router:find_by_name(structs.str_pack(request_header.From.Name))
-         print("router:::",pprint.format(route_info))
-         print(structs.str_pack(request_header.To.AppType))
-         local trade = require(structs.str_pack(request_header.To.AppType) .. ".server.request")
-         local app = trade.Request:new()
-         local logname =string.format("%s.log",route_info.app_type)
-         local logger = logger.new(logname)
-         -- local sqlite_path = configure.svc.db.sqlite
-         local __gala__ = {route_info=route_info,logger=logger,db_connection=sqlite_conn, _router=_router,  _header = request_header}
-         app:init(__gala__, request_body)
-         app:Run()
-         logger:finalizer()
-      end
+      local app = request_handle(request_header)
+      local logname =string.format("%s.log",structs.str_pack(request_header.To.AppType))
+      local _logger = logger.new(logname)
+      local __gala__ = {_router=_router,_logger=_logger,_db=dbconn, _header = request_header, _router_info = item}
+      app:init(__gala__, request_body)
+      app:Run()
+      _logger:finalizer()
       glr.send(dispatcherid, cjson.encode({gpid = __id__}))
    end
-   sqlite_conn:close()
+   dbconn:close()
 end
 
 
