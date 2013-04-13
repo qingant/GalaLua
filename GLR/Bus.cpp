77261ad4225c2675b35c521090ae5d8a08c6776f
matao <ma.tao@agree.com.cn>
2013-04-13 07:28:47 +0000

    修正中断完成时进程已被杀死导致崩溃的bug

--- GLR/Bus.cpp
+++ GLR/Bus.cpp
@@ -105,59 +105,66 @@ void GLR::Bus::Send( int pid, const std::string &msg, MSG_HEAD::MSG_TYPE type)
 void Bus::Response( int pid, int narg, ...)
 {
 
-    Process &nd = Runtime::GetInstance().GetProcess(pid);
-    Galaxy::GalaxyRT::CLockGuard _Gl(&nd._IntLock);
 
-    GALA_ERROR("Pid(%d) Status(%d)", pid, nd._Status._State);
-    if (nd._Status._State != Process::ProcessStatus::INT_WAIT)
+    try
     {
-        THROW_EXCEPTION_EX("Fatal Error");
-    }
-    int type = 0;
-    const char *str = NULL;
-    int number = 0;
-    va_list argp;
-    va_start(argp, narg);
+        Process &nd = Runtime::GetInstance().GetProcess(pid);
 
-    for (int i = 0; i!=narg; ++i)
-    {
-        type = va_arg(argp, int);
-        switch (type)
+        GALA_ERROR("Pid(%d) Status(%d)", pid, nd._Status._State);
+        if (nd._Status._State != Process::ProcessStatus::INT_WAIT)
         {
-        case LUA_TSTRING:
-            str = va_arg(argp, const char*);
-            number = va_arg(argp, int);
-            if (number == 0)
-            {
-                lua_pushlstring(nd._Stack, str, strlen(str));
-            }
-            else
-            {
-                lua_pushlstring(nd._Stack, str, number);
-            }
-
-            break;
-        case LUA_TNUMBER:
-            number = va_arg(argp, int);
-            lua_pushinteger(nd._Stack, number);
-            break;
-        case LUA_TNIL:
-            lua_pushnil(nd._Stack);
-            break;
-        case LUA_TBOOLEAN:
-            number = va_arg(argp, int);
-            lua_pushboolean(nd._Stack, number);
-            break;
-        default:
-            break;
+            THROW_EXCEPTION_EX("Fatal Error");
         }
+        int type = 0;
+        const char *str = NULL;
+        int number = 0;
+        va_list argp;
+        va_start(argp, narg);
+
+        for (int i = 0; i!=narg; ++i)
+        {
+            type = va_arg(argp, int);
+            switch (type)
+            {
+            case LUA_TSTRING:
+                str = va_arg(argp, const char*);
+                number = va_arg(argp, int);
+                if (number == 0)
+                {
+                    lua_pushlstring(nd._Stack, str, strlen(str));
+                }
+                else
+                {
+                    lua_pushlstring(nd._Stack, str, number);
+                }
+
+                break;
+            case LUA_TNUMBER:
+                number = va_arg(argp, int);
+                lua_pushinteger(nd._Stack, number);
+                break;
+            case LUA_TNIL:
+                lua_pushnil(nd._Stack);
+                break;
+            case LUA_TBOOLEAN:
+                number = va_arg(argp, int);
+                lua_pushboolean(nd._Stack, number);
+                break;
+            default:
+                break;
+            }
+        }
+
+        va_end(argp);
+
+        nd._Status._NArg = narg;
+        nd._Status._State = Process::ProcessStatus::INT_RESP;  
+        Runtime::GetInstance().GetSchedule().PutTask(nd);
     }
-
-    va_end(argp);
-
-    nd._Status._NArg = narg;
-    nd._Status._State = Process::ProcessStatus::INT_RESP;  
-    Runtime::GetInstance().GetSchedule().PutTask(nd);
+    catch (const Galaxy::GalaxyRT::CException &e)
+    {
+        return;
+    }
 
 }
 
