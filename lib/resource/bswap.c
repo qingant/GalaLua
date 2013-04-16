7b6316a2f3de8849bfaa29d2c3d6f50d7fcabc8e
libo1218 <li.bo@agree.com.cn>
2013-04-16 10:05:57 +0000

    资源打包库中bswap.h和bswap.c文件的条件编译处理。

--- lib/resource/bswap.c
+++ lib/resource/bswap.c
@@ -12,11 +12,11 @@
 #include <unistd.h>
 
 #ifdef __linux__
-#include <endian.h>
+  #include <endian.h>
 #elif defined(__aix__)
-#include <sys/machine.h>
+  #include <sys/machine.h>
 #elif defined(__freebsd__)
-#include <sys/endian.h>
+  #include <sys/endian.h>
 #endif
 
 #include "bswap.h"
@@ -26,181 +26,183 @@ extern "C"
 {
 #endif
 
-//#if defined(__aix__)
-#if BYTE_ORDER == BIG_ENDIAN
+#ifndef __freebsd__
+#  if defined(__aix__) || (defined(__linux__) && (__GLIBC__ < 2 || __GLIBC_MINOR__ < 9))
+#    if BYTE_ORDER == BIG_ENDIAN
 
-inline uint16_t htobe16(const uint16_t h16)
+uint16_t htobe16(const uint16_t h16)
 {
     return h16;
 }
 
-inline uint16_t htole16(const uint16_t h16)
+uint16_t htole16(const uint16_t h16)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap16(h16);
 #else
-    return bswap_16(h16);
+    return bswap16(h16);
 #endif
 }
 
-inline uint16_t be16toh(const uint16_t be16)
+uint16_t be16toh(const uint16_t be16)
 {
     return be16;
 }
 
-inline uint16_t le16toh(const uint16_t le16)
+uint16_t le16toh(const uint16_t le16)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap16(le16);
 #else
-    return bswap_16(le16);
+    return bswap16(le16);
 #endif
 }
 
-inline uint32_t htobe32(const uint32_t h32)
+uint32_t htobe32(const uint32_t h32)
 {
     return h32;
 }
 
-inline uint32_t htole32(const uint32_t h32)
+uint32_t htole32(const uint32_t h32)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap32(h32);
 #else
-    return bswap_32(h32);
+    return bswap32(h32);
 #endif
 }
 
-inline uint32_t be32toh(const uint32_t be32)
+uint32_t be32toh(const uint32_t be32)
 {
     return be32;
 }
 
-inline uint32_t le32toh(const uint32_t le32)
+uint32_t le32toh(const uint32_t le32)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap32(le32);
 #else
-    return bswap_32(le32);
+    return bswap32(le32);
 #endif
 }
 
-inline uint64_t htobe64(const uint64_t h64)
+uint64_t htobe64(const uint64_t h64)
 {
     return h64;
 }
 
-inline uint64_t htole64(const uint64_t h64)
+uint64_t htole64(const uint64_t h64)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap64(h64);
 #else
-    return bswap_64(h64);
+    return bswap64(h64);
 #endif
 }
 
-inline uint64_t be64toh(const uint64_t be64)
+uint64_t be64toh(const uint64_t be64)
 {
     return be64;
 }
 
-inline uint64_t le64toh(const uint64_t le64)
+uint64_t le64toh(const uint64_t le64)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap64(le64);
 #else
-    return bswap_64(le64);
+    return bswap64(le64);
 #endif
 }
 
-#elif BYTE_ORDER == LITTLE_ENDIAN  // end of '#elif BYTE_ORDER == BIG_ENDIAN'
+#    elif BYTE_ORDER == LITTLE_ENDIAN  // end of '#elif BYTE_ORDER == BIG_ENDIAN'
 
-inline uint16_t htobe16(const uint16_t h16)
+uint16_t htobe16(const uint16_t h16)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap16(h16);
 #else
-    return bswap_16(h16);
+    return bswap16(h16);
 #endif
 }
 
-inline uint16_t htole16(const uint16_t h16)
+uint16_t htole16(const uint16_t h16)
 {
     return h16;
 }
 
-inline uint16_t be16toh(const uint16_t be16)
+uint16_t be16toh(const uint16_t be16)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap16(be16);
 #else
-    return bswap_16(be16);
+    return bswap16(be16);
 #endif
 }
 
-inline uint16_t le16toh(const uint16_t le16)
+uint16_t le16toh(const uint16_t le16)
 {
     return le16;
 }
 
-inline uint32_t htobe32(const uint32_t h32)
+uint32_t htobe32(const uint32_t h32)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap32(h32);
 #else
-    return bswap_32(h32);
+    return bswap32(h32);
 #endif
 }
 
-inline uint32_t htole32(const uint32_t h32)
+uint32_t htole32(const uint32_t h32)
 {
     return h32;
 }
 
-inline uint32_t be32toh(const uint32_t be32)
+uint32_t be32toh(const uint32_t be32)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap32(be32);
 #else
-    return bswap_32(be32);
+    return bswap32(be32);
 #endif
 }
 
-inline uint32_t le32toh(const uint32_t h32)
+uint32_t le32toh(const uint32_t h32)
 {
     return h32;
 }
 
-inline uint64_t htobe64(const uint64_t h64)
+uint64_t htobe64(const uint64_t h64)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap64(h64);
 #else
-    return bswap_64(h64);
+    return bswap64(h64);
 #endif
 }
 
-inline uint64_t htole64(const uint64_t h64)
+uint64_t htole64(const uint64_t h64)
 {
     return h64;
 }
 
-inline uint64_t be64toh(const uint64_t be64)
+uint64_t be64toh(const uint64_t be64)
 {
 #if defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 2
     return __builtin_bswap64(be64);
 #else
-    return bswap_64(be64);
+    return bswap64(be64);
 #endif
 }
 
-inline uint64_t le64toh(const uint64_t le64)
+uint64_t le64toh(const uint64_t le64)
 {
     return le64;
 }
 
-#endif // end of '#elif BYTE_ORDER == LITTLE_ENDIAN'
-//#endif
+#    endif // end of '#elif BYTE_ORDER == LITTLE_ENDIAN'
+#  endif // end of 'if defined(__aix__)'
+#endif // end of 'ifndef __freebsd__'
 
 #ifdef __cplusplus
 }
--- lib/resource/bswap.h
+++ lib/resource/bswap.h
@@ -14,43 +14,61 @@
 #include <inttypes.h>
 #include <sys/types.h>
 
-//#if defined(__linux__)
-//# if __GLIBC__ > 1 && __GLIBC_MINOR__ > 8
-//#   include <endian.h>
-//# else
-//#   include <byteswap.h>
-//# endif
-//#endif
+#if defined(__linux__)
+#  if __GLIBC__ > 1 && __GLIBC_MINOR__ > 8
+#    ifndef _BSD_SOURCE
+#      define _BSD_SOURCE
+#    endif
+#    include <endian.h>
+#  else
+#    include <byteswap.h>
+#  endif
+#elif defined(__freebsd__)
+   #include <sys/endian.h>
+#endif
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#ifndef bswap_16
-#define bswap_16(i16)    ((uint16_t)((((i16) & 0x00ff) << 8) \
-                            | (((i16) >> 8) & 0x00ff)))
+#ifndef bswap16
+#  ifdef bswap_16
+#    define bswap16(i16)    bswap_16(i16)
+#  else
+#    define bswap16(i16)    ((uint16_t)((((i16) & 0x00ff) << 8)         \
+                                | (((i16) >> 8) & 0x00ff)))
+#  endif
 #endif
 
-#ifndef bswap_32
-#define bswap_32(i32)    ((uint32_t)((((i32) & 0xff000000u) >> 24)  \
-                            | (((i32) & 0x00ff0000u) >> 8)          \
-                            | (((i32) & 0x0000ff00u) << 8)          \
-                            | (((i32) & 0x000000ffu) << 24)))
+#ifndef bswap32
+#  ifdef bswap_32
+#    define bswap32(i32)    bswap_32(i32)
+#  else
+#    define bswap32(i32)    ((uint32_t)((((i32) & 0xff000000u) >> 24)   \
+                                | (((i32) & 0x00ff0000u) >> 8)          \
+                                | (((i32) & 0x0000ff00u) << 8)          \
+                                | (((i32) & 0x000000ffu) << 24)))
+#  endif
 #endif
 
-#ifndef bswap_64
-#define bswap_64(i64)    ((uint64_t)((((i64) & 0xff00000000000000ull) >> 56)\
-                            | (((i64) & 0x00ff000000000000ull) >> 40)       \
-                            | (((i64) & 0x0000ff0000000000ull) >> 24)       \
-                            | (((i64) & 0x000000ff00000000ull) >> 8)        \
-                            | (((i64) & 0x00000000ff000000ull) << 8)        \
-                            | (((i64) & 0x0000000000ff0000ull) << 24)       \
-                            | (((i64) & 0x000000000000ff00ull) << 40)       \
-                            | (((i64) & 0x00000000000000ffull) << 56)))
+#ifndef bswap64
+#  ifdef bswap_64
+#    define bswap64(i64)    bswap_64(i64)
+#  else
+#    define bswap64(i64)    ((uint64_t)((((i64) & 0xff00000000000000ull) >> 56) \
+                                | (((i64) & 0x00ff000000000000ull) >> 40)       \
+                                | (((i64) & 0x0000ff0000000000ull) >> 24)       \
+                                | (((i64) & 0x000000ff00000000ull) >> 8)        \
+                                | (((i64) & 0x00000000ff000000ull) << 8)        \
+                                | (((i64) & 0x0000000000ff0000ull) << 24)       \
+                                | (((i64) & 0x000000000000ff00ull) << 40)       \
+                                | (((i64) & 0x00000000000000ffull) << 56)))
+#  endif
 #endif
 
-//#if defined(__aix__)
+#ifndef __freebsd__
+#  if (defined(__aix__) || (defined(__linux__) && (__GLIBC__ < 2 || __GLIBC_MINOR__ < 9)))
     uint16_t htobe16(uint16_t h16);
     uint16_t htole16(uint16_t h16);
     uint16_t be16toh(uint16_t be16);
@@ -65,7 +83,8 @@ extern "C"
     uint64_t htole64(uint64_t h64);
     uint64_t be64toh(uint64_t be64);
     uint64_t le64toh(uint64_t le64);
-//#endif
+#  endif
+#endif
 
 #ifdef __cplusplus
 }
