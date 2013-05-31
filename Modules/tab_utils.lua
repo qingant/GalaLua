503209fb9b89b30c6528eada2e8c1e7d35678279
matao <ma.tao@agree.com.cn>
2013-05-31 02:56:50 +0000

    add isArray

--- Modules/tab_utils.lua
+++ Modules/tab_utils.lua
@@ -7,3 +7,22 @@ local function update(tab1, tab2)
 	end
 	return tab1
 end
+table.update = update
+
+local function map(self, func, ... )
+    local result = {}
+    for i,v in ipairs(self) do
+        result[#result+1] = func(self,...)
+    end
+    return result
+end
+table.map = map
+
+local function isArray( this )
+    if this[1] then
+        return true
+    else
+        return false
+    end
+end
+table.isArray = isArray
