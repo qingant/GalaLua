a7177df5661e9fe7ac15903e9b9ef0ce8a80c368
Ma Tao <matao.xjtu@gmail.com>
2013-06-20 10:43:51 +0800

    Config init version

--- Config/config.lua
+++ Config/config.lua
@@ -1,8 +1,8 @@
 module(..., package.seeall)
 
 local xml = require "xml"
-local mdb = require "mdb"
-local xStr = "<root></root>"
+local mdb = require("mdb").mdb
+local xStr = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root></root>"
 local reader = xml.cxml_reader(xStr,#xStr)
 local root = reader:document()
 local _XML = getmetatable(root)
@@ -15,6 +15,9 @@ function _XML:getElementByTag( tag )
         end
     end
     return result
+end
+function _XML:isLeaf( ... )
+    return (#self:sub_elements() == 0)
 end
 
 local _Config = {}
@@ -30,27 +33,97 @@ end
 function _Config:init( path )
     local db = mdb:new():init(mdb.create_env(path))
     self._db = db
+
     return self
 end
-
+function _Config:prepairPath( xpath )
+    local path = string.gsub(xpath, "/[^/]*%[0*([0-9])%]/", "/%1/")
+    return path
+end
 function _Config:get( xpath )
+    local path = self:prepairPath(xpath)
+    print(path)
     return self._db:withReadOnly(function (db)
-                                 local vs = db:xpath(xpath).get_value()
-                                 return vs
+                                 local root = db:get_root("Configure"):get_child("Contents")
+
+                                 local vs = root:xpath(path)[1]:get_value()
+                                 return vs[1]
                                  end)
 end
 function _Config:put( xpath, value )
+    local path = self:prepairPath(xpath)
+
     self._db:with(function ( db )
-        local num = db:xpath_set(xpath, value)
-        if num == 0 then
-            error("No record altered")
-        end
-        return num
+                  local root = db:get_root("Configure"):get_child("Contents")
+                  local node = root:xpath(path)[1]
+                  for k,v in pairs(node:get_value()) do
+                    node:remove_value(v)
+                  end
+                  node:add_value(value)    
+                  db:commit()              
     end)
 end
 
 function _Config:schemaGen( ... )
+    self._db:with(function ( db )
+        local root = db:get_root("Configure")
+        local contents = root:add_node("Contents")
+        local schema = db:get_root("Schema")
+        local sStructure = schema:get_child("_STRUCTURE"):get_child()
+        for k,v in pairs(sStructure) do
+            sStructure = v
+        end
 
+        function doSchema( sNode, cNode )
+
+            local meta = sNode:get_attrib()["meta"]
+            print("DoSchema", sNode:tag(), cNode:tag(), meta)
+            if meta == "entry" then
+                local eType = sNode:get_child("type"):get_value()[1]
+                cNode:add_attrib("type", eType)
+                if eType == "path" or eType == "ip" or eType == "string" then
+                    local maxLen = sNode:get_child("size"):get_value()[1]
+                    --maxLen = tostring(maxLen)
+                    cNode:add_attrib("maxLen", maxLen)
+                elseif eType == "int" then
+                    local limit = sNode:get_child("limit") 
+                    if limit then
+                        local max = limit:get_child("max") and limit:get_child("max"):get_value()[1] or "0"
+                        local min = limit:get_child("min") and limit:get_child("min"):get_value()[1] or "0"
+                        cNode:add_attrib("max", max)
+                        cNode:add_attrib("min",min)
+                    end
+                elseif eType == "enum" then
+                    local sRoot = sNode:get_root()
+
+                end
+
+
+
+
+            elseif meta == "struct" then
+                for k, v in pairs(sNode:get_child()) do
+                    local c = cNode:add_node(v:tag())
+                    doSchema(v, c)
+                end
+
+            elseif meta == "array" then
+                local aStruct
+                for k,v in pairs(sNode:get_child()) do
+                    aStruct = v
+                end
+                local vNode = cNode:add_vector_node(sNode:tag(), aStruct:tag())
+                local limit = tostring(sNode:get_attrib()["count"])
+                for i=0,limit-1 do
+                    local item = vNode:add_vector_item()
+                    doSchema(aStruct, item)
+                end
+            end
+        end
+
+        doSchema(sStructure, contents)
+        db:commit()
+    end)
 end
 
 local _Importer = {
@@ -63,33 +136,65 @@ function _Importer:new( o )
     return o
 end
 function _Importer:importFromXML( path , root)
-    local f=io.open(path,"r")
+    local t=io.open(path,"r"):read("*a")
     local reader = xml.cxml_reader(t,#t)
     local xRoot = reader:document()
     local dRoot = self._db:get_root(root)
-
+    self._xRoot = xRoot
+    self._dRoot = dRoot
+    self._db:with(
+                  function (db)
+                      self:_import(xRoot, dRoot)
+                      db:commit()
+                  end
+                  )
 
 end
 
+function _Importer:showResult(  )
+    self._db:withReadOnly(function ( ... )
+                          local s = io.open("out.xml","w")
+                          s:write(self._dRoot:to_xml())
+                          s:flush()
+                          self._dRoot:show()
+    end
+    )
+end
 function _Importer:_import( xElement, dElement )
     for k,v in pairs(xElement:properties()) do
         dElement:add_attrib(k, v)
     end
 
     for i,v in ipairs(xElement:sub_elements()) do
+        print("VK",v:key())
         local e = dElement:add_node(v:key())
         self:_import(v, e)
     end
-
+    if #xElement:sub_elements() > 0 then
+        return
+    end
     if xElement:value() ~= "" then
+        local v = xElement:value()
+        assert(string.sub(v, 1, 1) ~= "<", v)
         dElement:add_value(xElement:value())
     end
+    return dElement
 end
 
 
 if ... == "__main__" then
+    local path = "/tmp/test_config"
+    os.execute(string.format("rm -rf %s && mkdir -p %s", path, path))
     local db = mdb:new():init(mdb.create_env(path))
     local imp = _Importer:new{_db = db}
-    local config_path = "/vagrant/code/GalaLua/"
-
+    local config_path = "./sample.xml"
+    imp:importFromXML(config_path, "Schema")
+    imp:showResult()
+    local config = _Config:new():init(path)
+    config:schemaGen()
+    config:put("MonitorList/1/Base/AMQToken", "test")
+    local tk = config:get("MonitorList/1/Base/AMQToken")
+    print(tk)
+    config:put("MonitorList/1/Base/AMQToken", "testABC")
+    print(config:get("MonitorList/1/Base/AMQToken"))
 end
