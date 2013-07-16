2428c3c3389afa709849eb929880451053e1816a
zhaozineng <zhao.zn@agree.com.cn>
2013-07-16 22:26:55 +0800

    no need to recompile GLR when modify the gar loader

--- GLR/glr.lua
+++ GLR/glr.lua
@@ -206,53 +206,7 @@ function get_option(key)
 	return arg_tab[key]
 end
 
-function run_gar(gar)
-    local cjson=require "cjson"
-    local zip=require "minizip"
-
-    function read_module_cache(zipfd)
-        local manifest="module.cache"
-        local f=zipfd:open(manifest)
-        local size=zipfd:stat(manifest).size
-        if size>0 then
-            return cjson.decode(f:read(size))
-        else
-            return {}
-        end
-    end
-    --search module in zipfd
-    --@mod:module
-    function search_module(zipfd,mod)
-        local max_id=zipfd:get_num_files()
-        if max_id then
-            for i=1,max_id do
-                local file=zipfd:get_name(i)
-                local module_name=file:match("/*([^/]+)%.lua$") 
-                if module_name==mod then
-                    return file
-                end
-            end
-        end
-    end
-
-    -- gar loader
-    function loader(modulename)
-        local gar=gar
-        local zipfd=zip.open(gar)
-        local file=read_module_cache(zipfd)[modulename] or search_module(zipfd,modulename)
-        if file then
-            local fstream=zipfd:open(file)
-            local str=fstream:read(zipfd:stat(file).size)
-            fstream:close()
-            return assert(loadstring(str))
-        end
-        zipfd:close()
-        return nil
-    end
-
-    table.insert(package.loaders,2,loader)
-
-end
+local gar=require "gar_loader"
+run_gar=gar.run_gar
 
 return M
-
---
+++ Modules/gar_loader.lua
@@ -0,0 +1,66 @@
+--[[
+ gar loader
+]]
+
+module(...,package.seeall)
+
+function run_gar(gar)
+    local zip=require "minizip"
+    local xml=require "xml"
+
+    local Manifest="Manifest"
+
+    function read_file(zipfd,file)
+        local f=zipfd:open(file)
+        if f then
+            local size=zipfd:stat(file).size
+            return f:read(size)
+        end
+    end
+    
+    --get Path tag in manifest.
+    function get_gpath(str)
+        local path="?.lua"
+        local exec="Excutable"   --default is runnable
+        if str then
+            local doc=xml.cxml_reader(str,#str)
+            local root=doc:document()
+            for i,elm in pairs(root:sub_elements()) do
+                local k,v=elm:key(),elm:value()
+                if k=="Path" then
+                    path=v
+                elseif k=="Catagory" then
+                    exec=v
+                end
+            end
+        end
+        
+        if exec~="Excutable" then
+            error("not a excutable gar package")
+        end
+
+        return path
+    end
+
+    -- gar loader
+    function loader(modulename)
+        local gar=gar
+        local zipfd=zip.open(gar)
+        local gpath=get_gpath(read_file(zipfd,Manifest))
+
+        local real_mod=string.gsub(modulename,"%.","/")
+
+        for path in string.gmatch(gpath, "([^;]+)") do
+            local file=string.gsub(path,"%?",real_mod)
+            local f=zipfd:open(file)
+            if f then
+                return assert(loadstring(f:read(zipfd:stat(file).size)))
+            end
+        end
+        return nil
+    end
+
+    table.insert(package.loaders,2,loader)
+
+end
+
