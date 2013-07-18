87ea0ff1662c421170c3ed47466728777488dea9
zhaozineng <zhao.zn@agree.com.cn>
2013-07-18 14:31:51 +0800

    add kill command to spyer

--- Modules/spyer.lua
+++ Modules/spyer.lua
@@ -10,6 +10,7 @@ local string =require  "string"
 function main()
     --[[
     --  receive protocol:
+    --  1) for getting some informations
     --  {
     --      Type:NODE
     --      Action:GET
@@ -19,6 +20,12 @@ function main()
     --      Nonstop:true/false   --directly send to ToAddr or backtrack to sender(ctr)
     --  }
     --
+    --  2) for run Cmds
+    --  {
+    --      Type:NODE
+    --      Action:EXEC
+    --      Cmd:kill
+    --  }
     --  response protocol:
     --  {
     --      Type:NODE
@@ -33,46 +40,55 @@ function main()
         local msg_type, addr, msg = glr.recv()
         msg_table =cjson.decode(msg) 
 
-        if msg_table.Type=="NODE" and msg_table.Action=="GET" then
-            local err,gpids=glr.status.processes()
-            assert(err,gpids)
-            pprint.pprint(gpids,"status::gpids")
-            local msg={Type="NODE",Action="RES",Cmd=msg_table.Cmd}
-            
-            -- want to take the direct train?
-            if msg_table.Nonstop then 
-                addr=msg_table.ToAddr
-            else
-                msg.ToAddr=msg_table.ToAddr
-            end
-            
-            pprint.pprint(addr,"AAAAAAA")
-            if msg_table.Cmd=="gpid" then
-                msg.Content=gpids
-                glr.send(addr,cjson.encode(msg))
-            elseif msg_table.Cmd=="status" then
-                local gpid_list=msg_table.Gpids or gpids
-                if #gpid_list==0 then
-                    gpid_list=gpids 
+        if msg_table.Type=="NODE" then
+            if msg_table.Action=="EXEC" then
+                if msg_table.Cmd=="kill" then
+                    glr.exit()
                 end
-                local content={}
-                for i,gpid in pairs(gpid_list) do
-                    err,content[tostring(gpid)]=glr.status.status(gpid)
+            elseif msg_table.Action=="GET" then
+                local err,gpids=glr.status.processes()
+                assert(err,gpids)
+                pprint.pprint(gpids,"status::gpids")
+                local msg={Type="NODE",Action="RES",Cmd=msg_table.Cmd}
+
+                -- want to take the direct train?
+                if msg_table.Nonstop then 
+                    addr=msg_table.ToAddr
+                else
+                    msg.ToAddr=msg_table.ToAddr
                 end
-                msg.Content=content
-                pprint.pprint(msg,"RESPONSETOCNR")
-                glr.send(addr,cjson.encode(msg))
-            elseif msg_table.Cmd=="pid" then
-                local content={pid=osext.getpid()}
-                msg.Content=content
-                glr.send(addr,cjson.encode(msg))
+
+                pprint.pprint(addr,"AAAAAAA")
+                if msg_table.Cmd=="gpid" then
+                    msg.Content=gpids
+                    glr.send(addr,cjson.encode(msg))
+                elseif msg_table.Cmd=="status" then
+                    local gpid_list=msg_table.Gpids or gpids
+                    if #gpid_list==0 then
+                        gpid_list=gpids 
+                    end
+                    local content={}
+                    for i,gpid in pairs(gpid_list) do
+                        err,content[tostring(gpid)]=glr.status.status(gpid)
+                    end
+                    msg.Content=content
+                    pprint.pprint(msg,"RESPONSETOCNR")
+                    glr.send(addr,cjson.encode(msg))
+                elseif msg_table.Cmd=="pid" then
+                    local content={pid=osext.getpid()}
+                    msg.Content=content
+                    glr.send(addr,cjson.encode(msg))
+                else
+                    msg.Content={"Not valid Command"}
+                    glr.send(addr,cjson.encode(msg))
+                end
             else
-                msg.Content={"Not valid Command"}
-                glr.send(addr,cjson.encode(msg))
+                pprint.pprint(msg_table,"Get Unkown Action Message")
+                glr.send(addr,cjson.encode({content='Unkown Action',msg=msg}))  
             end
         else
-            print("Hey,You should not send messages to me !")
-            glr.send(addr,"{'content':'Hey,You should not send messages to me !'}")  
+            pprint.pprint(msg_table,"Get Unkown Message")
+            glr.send(addr,cjson.encode({content='Unkown Message',msg=msg}))  
         end
     end
 end
