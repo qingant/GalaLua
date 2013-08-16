f46bb99eb6b940d6010877e5515cc5860764772b
Ma Tao <matao.xjtu@gmail.com>
2013-08-16 15:19:50 +0800

    new log server with a dispacher

--- Modules/log_server.lua
+++ Modules/log_server.lua
@@ -1,5 +1,6 @@
 module(..., package.seeall)
 local cjson = require("cjson")
+local pprint = require("pprint")
 local memcached = require("memcached").GlrBuffer
 --获取文件大小
 local function fSize( file )
@@ -26,25 +27,80 @@ local function getFileNameWithoutExtension(filename)
     end
 end
 
-
 --获取扩展名
 function getFileExtension(filename)
     return filename:match(".+%.(%w+)$")
 end
+function timedFlusher(timeVal, pid)
+    while true do
+        print("flusher")
+        glr.time.sleep(5)
+        glr.send(pid, "!!")     -- flush
+    end
+end
+function glrLogServerDispatch(info)
+    local info = cjson.decode(info)
+    local _, timer = glr.spawn("log_server", "timedFlusher", 30, __id__)
+    local nameDict = {}
+    local pidDict = {}
+    while true do
+        repeat
+            local msgType, addr, msg = glr.recv()
+            if msgType == glr.EXIT then
+                local key = pidDict[addr.gpid]
+                if key ~= nil then
+                    nameDict[key] = nil
+                    pidDict[addr.gpid] = nil
+                end
+                break
+            elseif msgType ~= glr.APP then
+                break
+            end
+            if msg:sub(1,2) == "!@" then -- open logger
+                print(msg)
+                -- local len = #msg - 2
+                local entry = string.sub(msg, 3, #msg)
+                local info = cjson.decode(entry)
+                pprint.pprint(info)
+                if info.path == nil or info.path == "" then
+                    info.path = "stdout"
+                    if nameDict["stdout"] == nil then
+                        local rt, pid = glr.spawn("log_server", "glrStdLoggerProcessor")
+                        nameDict["stdout"] = pid
+                    end
+                elseif nameDict[info.path] == nil then
+                    local rt, pid = glr.spawn("log_server", "glrFileLoggerProcessor",info.path,info.size,info.copys)
+                    nameDict[info.path] = pid
+                    pidDict[pid] = info.path
+                end
+                local childaddr = {}
+                childaddr.host = glr.sys.host
+                childaddr.port = glr.sys.port
+                childaddr.gpid = nameDict[info.path]
+                glr.send(addr,cjson.encode(childaddr))
+            elseif msg:sub(1,2) == "!!" then -- flush
+                for pid, name in pairs(pidDict) do
+                    print("flush", pid, name)
+                    glr.send(pid, "!!")
+                end
+            end
+        until true
+    end
+end
+function glrStdLoggerProcessor()
+    while true do
+        local msgType, addr, msg = glr.recv()
+        if msgType ~= glr.APP then
+            break
+        end
+        if msg:sub(1,2) == "!!" then
+        else
+            print(msg)
+        end
+    end
+end
 
-
--- function glrLoggerServer()
---     while true do
---         local msgType, addr, msg = glr.recv()
---         local msgTable = cjson.decode(msg)
---         if msgType == glr.APP then
---             if msgType.Type == "open" then
---             elseif msgType.Type == "close"
---             end
---         end
---     end
--- end
-function glrLoggerProcessor( targetPath,sizePerFile,Files)
+function glrFileLoggerProcessor( targetPath,sizePerFile,Files)
     local filepath = targetPath
     local size = sizePerFile
     local files = Files
@@ -59,7 +115,7 @@ function glrLoggerProcessor( targetPath,sizePerFile,Files)
         if filesize  >= size then
             fileid = fileid % files
             local strpath = string.format("%s_%d.%s",getFileNameWithoutExtension(filepath),
-	            fileid,getFileExtension(filepath))
+                                          fileid,getFileExtension(filepath))
             fileHandle:close()
             os.rename(filepath,strpath)
             fileHandle = io.open(filepath, "a")
@@ -67,6 +123,7 @@ function glrLoggerProcessor( targetPath,sizePerFile,Files)
             fileid = fileid + 1
         end
         if msg:sub(1,2) == "!!" or msg:sub(1,2) == "!#" then -- flush
+            print("flush!")
             --fileHandle:flush()
             local cachedata = cache:get()
             for _, v in pairs(cachedata) do
@@ -98,41 +155,4 @@ function glrLoggerProcessor( targetPath,sizePerFile,Files)
     end
 end
 
-GlrLoggerAppender = {}
 
-function GlrLoggerAppender:new()
-    local o = {}
-    setmetatable(o, self)
-    self.__index = self
-    return o
-end
-
-function GlrLoggerAppender:init( path,size,copys )
-    local size = size or 64*1024*1024
-    local copys = copys or 2000
-    local rt, pid = glr.spawn("log_server", "glrLoggerProcessor", path,size,copys)
-    self._appender = pid
-    self.isclosed = false
-    return self
-end
-function GlrLoggerAppender:reinit(pid,flag)
-    self._appender = pid
-    self.isclosed = flag
-    return self
-end
-function GlrLoggerAppender:print( msg )
-    if self.isclosed == false then
-        glr.send(self._appender, msg)
-    end
-end
-function GlrLoggerAppender:flush(  )
-    if self.isclosed == false then
-        glr.send(self._appender, "!!")
-    end
-end
-function GlrLoggerAppender:close()
-    if self.isclosed == false then
-        glr.send(self._appender, "!#")
-	self.isclosed = true
-    end
-end
