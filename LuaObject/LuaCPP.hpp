4981b56219a3bc1ad20a77db769bb3ff636ba278
libo <libo1218@cfischina.com>
2013-08-19 15:56:15 +0800

    将LuaCPP.hpp中的函数setfield_int和setfield_string定义为宏，避免使用静态库时出现的错误。例如静态库A和B都包含了LuaCPP.hpp，那么当链接时同时需要A和B时，编译器便会报错。

--- LuaObject/LuaCPP.hpp
+++ LuaObject/LuaCPP.hpp
@@ -24,17 +24,20 @@ catch(...)\
 }
 
 //t[k]=v ,table t is on the top of stack
-void setfield_int(lua_State *L,int v,const char *k)
-{
-    lua_pushnumber(L,v);
-    lua_setfield(L,-2,k);
-}
+#ifndef setfield_int
+#define setfield_int(L, v, k)   do {        \
+        lua_pushnumber((L), (v));           \
+        lua_setfield((L), -2, (k));         \
+    } while (0)
+#endif
+
 //t[k]=v ,table t is on the top of stack,argument "v" must be a zero-terminated string
 //we can't use lua_pushlstring here,because it will push exactly "len" chars to lua(including '\0')
-void setfield_string(lua_State *L,const char *v,const char *k)
-{
-    lua_pushstring(L,v);
-    lua_setfield(L,-2,k);
-}
+#ifndef setfield_string
+#define setfield_string(L, v, k)    do {    \
+        lua_pushstring((L), (v));           \
+        lua_setfield((L), -2, (k));         \
+    } while (0)
+#endif
 
 #endif   //  __LUACPP_HPP__
