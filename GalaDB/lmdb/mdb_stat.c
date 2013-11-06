be9778832f56520a18a420600d96f12adcd047a2
zhaozineng <zhao.zn@agree.com.cn>
2013-11-06 09:21:08 +0800

    更新mdb源码至550bbe3788fafdf0fddcb2130173c1349fbc477f

--- GalaDB/lmdb/mdb_stat.c
+++ GalaDB/lmdb/mdb_stat.c
@@ -17,21 +17,27 @@
 #include <unistd.h>
 #include "lmdb.h"
 
+#ifdef	_WIN32
+#define	Z	"I"
+#else
+#define	Z	"z"
+#endif
+
 static void prstat(MDB_stat *ms)
 {
 #if 0
 	printf("  Page size: %u\n", ms->ms_psize);
 #endif
 	printf("  Tree depth: %u\n", ms->ms_depth);
-	printf("  Branch pages: %zu\n", ms->ms_branch_pages);
-	printf("  Leaf pages: %zu\n", ms->ms_leaf_pages);
-	printf("  Overflow pages: %zu\n", ms->ms_overflow_pages);
-	printf("  Entries: %zu\n", ms->ms_entries);
+	printf("  Branch pages: %"Z"u\n", ms->ms_branch_pages);
+	printf("  Leaf pages: %"Z"u\n", ms->ms_leaf_pages);
+	printf("  Overflow pages: %"Z"u\n", ms->ms_overflow_pages);
+	printf("  Entries: %"Z"u\n", ms->ms_entries);
 }
 
 static void usage(char *prog)
 {
-	fprintf(stderr, "usage: %s dbpath [-e] [-f[f[f]]] [-n] [-a|-s subdb]\n", prog);
+	fprintf(stderr, "usage: %s dbpath [-n] [-e] [-r[r]] [-f[f[f]]] [-a|-s subdb]\n", prog);
 	exit(EXIT_FAILURE);
 }
 
@@ -46,7 +52,7 @@ int main(int argc, char *argv[])
 	char *prog = argv[0];
 	char *envname;
 	char *subname = NULL;
-	int alldbs = 0, envinfo = 0, envflags = 0, freinfo = 0;
+	int alldbs = 0, envinfo = 0, envflags = 0, freinfo = 0, rdrinfo = 0;
 
 	if (argc < 2) {
 		usage(prog);
@@ -56,10 +62,11 @@ int main(int argc, char *argv[])
 	 * -s: print stat of only the named subDB
 	 * -e: print env info
 	 * -f: print freelist info
+	 * -r: print reader info
 	 * -n: use NOSUBDIR flag on env_open
 	 * (default) print stat of only the main DB
 	 */
-	while ((i = getopt(argc, argv, "aefns:")) != EOF) {
+	while ((i = getopt(argc, argv, "aefnrs:")) != EOF) {
 		switch(i) {
 		case 'a':
 			if (subname)
@@ -74,6 +81,9 @@ int main(int argc, char *argv[])
 			break;
 		case 'n':
 			envflags |= MDB_NOSUBDIR;
+			break;
+		case 'r':
+			rdrinfo++;
 			break;
 		case 's':
 			if (alldbs)
@@ -100,24 +110,38 @@ int main(int argc, char *argv[])
 		printf("mdb_env_open failed, error %d %s\n", rc, mdb_strerror(rc));
 		goto env_close;
 	}
-	rc = mdb_txn_begin(env, NULL, MDB_RDONLY, &txn);
-	if (rc) {
-		printf("mdb_txn_begin failed, error %d %s\n", rc, mdb_strerror(rc));
-		goto env_close;
-	}
 
 	if (envinfo) {
 		rc = mdb_env_stat(env, &mst);
 		rc = mdb_env_info(env, &mei);
 		printf("Environment Info\n");
 		printf("  Map address: %p\n", mei.me_mapaddr);
-		printf("  Map size: %zu\n", mei.me_mapsize);
+		printf("  Map size: %"Z"u\n", mei.me_mapsize);
 		printf("  Page size: %u\n", mst.ms_psize);
-		printf("  Max pages: %zu\n", mei.me_mapsize / mst.ms_psize);
-		printf("  Number of pages used: %zu\n", mei.me_last_pgno+1);
-		printf("  Last transaction ID: %zu\n", mei.me_last_txnid);
+		printf("  Max pages: %"Z"u\n", mei.me_mapsize / mst.ms_psize);
+		printf("  Number of pages used: %"Z"u\n", mei.me_last_pgno+1);
+		printf("  Last transaction ID: %"Z"u\n", mei.me_last_txnid);
 		printf("  Max readers: %u\n", mei.me_maxreaders);
 		printf("  Number of readers used: %u\n", mei.me_numreaders);
+	}
+
+	if (rdrinfo) {
+		printf("Reader Table Status\n");
+		rc = mdb_reader_list(env, (MDB_msg_func *)fputs, stdout);
+		if (rdrinfo > 1) {
+			int dead;
+			mdb_reader_check(env, &dead);
+			printf("  %d stale readers cleared.\n", dead);
+			rc = mdb_reader_list(env, (MDB_msg_func *)fputs, stdout);
+		}
+		if (!(subname || alldbs || freinfo))
+			goto env_close;
+	}
+
+	rc = mdb_txn_begin(env, NULL, MDB_RDONLY, &txn);
+	if (rc) {
+		printf("mdb_txn_begin failed, error %d %s\n", rc, mdb_strerror(rc));
+		goto env_close;
 	}
 
 	if (freinfo) {
@@ -154,20 +178,20 @@ int main(int argc, char *argv[])
 					pg += span;
 					for (; i >= span && iptr[i-span] == pg; span++, pg++) ;
 				}
-				printf("    Transaction %zu, %zd pages, maxspan %zd%s\n",
+				printf("    Transaction %"Z"u, %"Z"d pages, maxspan %"Z"d%s\n",
 					*(size_t *)key.mv_data, j, span, bad);
 				if (freinfo > 2) {
 					for (--j; j >= 0; ) {
 						pg = iptr[j];
 						for (span=1; --j >= 0 && iptr[j] == pg+span; span++) ;
-						printf(span>1 ? "     %9zu[%zd]\n" : "     %9zu\n",
+						printf(span>1 ? "     %9"Z"u[%"Z"d]\n" : "     %9"Z"u\n",
 							pg, span);
 					}
 				}
 			}
 		}
 		mdb_cursor_close(cursor);
-		printf("  Free pages: %zu\n", pages);
+		printf("  Free pages: %"Z"u\n", pages);
 	}
 
 	rc = mdb_open(txn, subname, 0, &dbi);
@@ -175,7 +199,7 @@ int main(int argc, char *argv[])
 		printf("mdb_open failed, error %d %s\n", rc, mdb_strerror(rc));
 		goto txn_abort;
 	}
-   
+
 	rc = mdb_stat(txn, dbi, &mst);
 	if (rc) {
 		printf("mdb_stat failed, error %d %s\n", rc, mdb_strerror(rc));
@@ -193,9 +217,12 @@ int main(int argc, char *argv[])
 			printf("mdb_cursor_open failed, error %d %s\n", rc, mdb_strerror(rc));
 			goto txn_abort;
 		}
-		while ((rc = mdb_cursor_get(cursor, &key, NULL, MDB_NEXT)) == 0) {
-			char *str = malloc(key.mv_size+1);
+		while ((rc = mdb_cursor_get(cursor, &key, NULL, MDB_NEXT_NODUP)) == 0) {
+			char *str;
 			MDB_dbi db2;
+			if (memchr(key.mv_data, '\0', key.mv_size))
+				continue;
+			str = malloc(key.mv_size+1);
 			memcpy(str, key.mv_data, key.mv_size);
 			str[key.mv_size] = '\0';
 			rc = mdb_open(txn, str, 0, &db2);
@@ -213,6 +240,9 @@ int main(int argc, char *argv[])
 		}
 		mdb_cursor_close(cursor);
 	}
+
+	if (rc == MDB_NOTFOUND)
+		rc = MDB_SUCCESS;
 
 	mdb_close(env, dbi);
 txn_abort:
