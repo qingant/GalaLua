c1efcfcd6b9aced8bc5a39cb2c084301ba5b58b6
zhaozineng <zhao.zn@agree.com.cn>
2014-01-13 16:24:53 +0800

    加载模块测试脚本

--- Test/test_module.lua
+++ Test/test_module.lua
@@ -1,24 +1,8 @@
 print(_VERSION)
 local pprint=require "pprint"
-local cjson=require "cjson"
-local amq=require "amq"
-local xml=require "xml"
-local plat=require "platform"
-local _dir=require "_dir"
-local osext=require "osext"
-local hex=require "hexdump"
-
-local struct=require "struct"
-local codec=require "codec"
-local lfs=require "lfs"
-local lightningmdb=require "lightningmdb"
-local linenoise=require "linenoise"
-local md5=require "md5"
-local minizip=require "minizip"
-local osdatetime=require "osdatetime"
-local sigar=require "sigar"
-
-local mods={"cjson","amq","xml","platform","_dir","osext","hexdump","struct","codec","lfs","lightningmdb","linenoise","md5","osdatetime","sigar"}
+local mods={"cjson","amq","xml","platform","_dir","osext","hexdump",
+            "struct","codec","lfs","lightningmdb","linenoise","md5",
+            "osdatetime","sigar","snappy"}
 
 function pmodule(m)
     local mod=require(m)
