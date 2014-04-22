42c9e520ee783ef2af1bc17ea7afe6269fb89db9
Ma Tao <ma.tao@cfischina.com>
2014-04-22 14:12:47 +0800

    delete junk folders

--- ozip/common.mk
+++
@@ -1,6 +0,0 @@
-CC			= gcc
-COMMINC		= -I. -I../LuaJIT-2.0.0/src/
-LNKFLAG		= -shared 
-COMPWARN	= -Wall -Wextra 
-CMPFLAG		= -O3 -c -g  -fPIC -pthread $(COMPWARN) $(COMMINC) -D_DEBUG_ -D_UNIX95_ -Wno-write-strings  -fno-strict-aliasing  -Wno-missing-field-initializers 
-SYTMLIB		= -lc -lz -lm -L.
