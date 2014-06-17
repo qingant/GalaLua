56a2622da99f299095379ed4154ba75a5ad66ece
Ma Tao <ma.tao@cfischina.com>
2014-06-17 16:15:38 +0800

    修改glr.status.processes返回值风格

--- GLR/LuaNode.cpp
+++ GLR/LuaNode.cpp
@@ -920,7 +920,6 @@ int Process::AllProcesses(lua_State *l)
 {
     Galaxy::GalaxyRT::CRWLockAdapter _RL(ProcessMapLock, Galaxy::GalaxyRT::CRWLockInterface::RDLOCK);
     Galaxy::GalaxyRT::CLockGuard _Gl(&_RL);
-    lua_pushboolean(l, 1);
     lua_newtable(l);
     for (size_t i = 0; i != NodeMap.size(); ++i)
     {
@@ -932,7 +931,7 @@ int Process::AllProcesses(lua_State *l)
         }
 
     }
-    return 2;
+    return 1;
 }
 
 
