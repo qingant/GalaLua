7835ef95130f352715407505080cf3c552c4c8f6
Ma Tao <ma.tao@cfischina.com>
2014-06-22 23:40:26 +0800

    加入kernel应用

---
+++ Frameworks/kernel/app.lua
@@ -0,0 +1,15 @@
+--[[
+    Author : Ma Tao
+	Date   : 2014/06/22
+
+	== Introduction ==
+    Gala Application Framework Kernel App Definition
+]]
+
+module(..., package.seeall)
+
+
+function on_started()
+    print("Kernel app started")
+end
+
