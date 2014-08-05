a3a38c9bcb74e2c2a1d13c75538f68cbcb6b0ec8
杨博 <yangbo@cfischina.com>
2014-08-05 14:00:24 +0800

    日志功能结构修改

--- Frameworks/core/logger.lua
+++ Frameworks/core/logger.lua
@@ -8,7 +8,7 @@ local rpc = require(_PACKAGE .. "rpc")
 local c_flag = require("c_flag")                               --added for c flags
 local bit = require("bit")
 local glr = require("glr")
-local cio = require(_PACKAGE .. "io")
+local cio = require("native.io")
 assert(require("str_utils"), "Cannot Import str_utils")
 assert(require("tab_utils"),"Cannot Import tab_utils")
 
@@ -24,7 +24,7 @@ local _logger = {
 }
 local _logger_Flag ={"DEBUG", "TRACE", "INFO ","WARN ", "ERROR", "FATAL"}
 logger = _logger
-_logger.format = pprint.format
+
 function _logger:new(o)
     local o = o or {}
     setmetatable(o, self)
@@ -43,10 +43,11 @@ function _logger:init(process,log_path)
     --self._file_size = 4*1024*1024
     self._file_size = 5000
     self._max_file_num = 10
-    self._times_to_get_filesize = 500 --每_times_to_get_filesize次flush buf，检查一次文件大小；为了测试方便，这里暂时设为1
+    self._times_to_get_filesize = 1 --每_times_to_get_filesize次flush buf，检查一次文件大小；为了测试方便，这里暂时设为1
     self._times = 0
+    --self._fd=cio.open(self._log_path,bit.bor(c_flag.O_CREAT,bit.bor(c_flag.O_APPEND,c_flag.O_RDWR)))
     self._output = print
-    self:set_path(log_path)
+    self:set_path(self._log_path)
     --self._timeout = 3
     return self
 end
@@ -61,14 +62,20 @@ function _logger:set_path(path)
 end
 
 function _logger:__gc()
-    if -1 ~= self._fd then
-        local rt = cio.close(self._fd)
-        if 0 ~= rt then
-            print("destruct logger io failed!")
-        else
-            print("destructed!")
-        end
+    self:finalize()
+end
+
+function _logger:finalize()
+    self:flush()
+    if -1 == self._fd then
+        return
     end
+    local rt = cio.close(self._fd)
+    if rt ~= 0 then
+        print("destruct logger io failed!")
+        return
+    end
+    self._fd = -1
 end
 
 function _logger:flush()
@@ -95,46 +102,56 @@ function _logger:_log_full(file_path)
     end
 end
 
+function _logger:_new_c_buf(structure,num,buf)
+    local iov = cio.new(structure, num)
+    for i=0,num-1 do
+        iov[i].iov_len=string.len(buf[i+1])
+        local ptr=cio.cast("char *",buf[i+1])
+        iov[i].iov_base=ptr
+    end
+    return iov
+end
+
 function _logger:_wirte_v() -- used for text
     local buf = self._buf
     local fd = self._fd
-    if buf ~= nil then -- 定时写入时检测是否需要
-        local n = #buf
-        if n ~= 0 then -- 异常保护
-            local iov = cio.new("iovec[?]", n)
-             for i=0,n-1 do
-                iov[i].iov_len=string.len(buf[i+1])
-                local ptr=cio.cast("char *",buf[i+1])
-                iov[i].iov_base=ptr
-            end
-            --local path=io.cast("char *",file_path)
-            --local fd=io.open(file_path,bit.bor(c_flag.O_CREAT,bit.bor(c_flag.O_APPEND,c_flag.O_RDWR)));
-            if -1 ~= fd then
-                res=cio.writev(fd,iov,n)
-                if -1 == res then
-                    print("write failed")
-                end
-                self._buf = {}
-            end
+    local n = #buf
+    if 0 == n then  -- there is no data to flush
+        return
+    end
+    --[[
+    local iov = cio.new("iovec[?]", n)
+    for i=0,n-1 do
+        iov[i].iov_len=string.len(buf[i+1])
+        local ptr=cio.cast("char *",buf[i+1])
+        iov[i].iov_base=ptr
+    end 
+    ]]
+    local iov = self:_new_c_buf("iovec[?]", n, buf)  
+    if fd ~= -1 then
+        res=cio.writev(fd,iov,n)
+        if -1 == res then
+            print("write failed")
         end
+        self._buf = {}
     end
 end
 
 function _logger:_write(msg)
     self._buf[#self._buf + 1]=msg
     self._buf_len = self._buf_len + string.len(msg)
+    if self._times >= self._times_to_get_filesize then
+        self._times = 0
+        self:_log_full(self._log_path)
+    end
     self._times=self._times + 1
     if self._buf_len >= self._buf_max_len  then
         self:flush()
     end
-    if self._times >= self._times_to_get_filesize then
-        self:_log_full()
-    end
-
 end
 --[[
 function _logger:_write_log(file_path) --used for slog
-        local writer = slog.create_writer(file_path)          --打开0.log日志文件,added for test
+        local writer = slog.creater(file_path)          --打开0.log日志文件,added for test
         if nil == writer then
             print("creat writer failed")
         end
@@ -172,9 +189,10 @@ function _logger:_log(level, format, ...)
             log_str= ("[%(level)s] [%(short_src)s:%(currentline)s] [G:%(gpid)s]:%(msg)s" % info)
         end
 
-        self:_log_local(time_format .. log_str)
+        self:_log_local(time_format .. log_str .. "\n")
+        --self._log_client:call("log", {level=info.level,msg=time_format..log_str.."\n"})
         if level >= self.enum_WARN then   --used for warn
-            --self._log_client:call("log", {level=info.level,msg=time_format.."\n"..log_str})
+            --self._log_client:call("log", {level=info.level,msg=time_format..log_str.."\n"})
         end
     end
 end
@@ -201,7 +219,6 @@ function _logger:fatal(format, ...)
 end
 function _logger:close()
     self:flush()
-    glr.send(self._handler,"!#")
 end
 function _logger:reset()
    glr.send(logPid,"!*")
