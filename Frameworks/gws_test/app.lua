b3b75623e817517b1150cd15304eb0e94eae5b7b
wenxueliu <wenxueliu73@gmail.com>
2014-08-08 17:25:33 +0800

    fix gws_test/app.lua static_path bug

--- Frameworks/gws_test/app.lua
+++ Frameworks/gws_test/app.lua
@@ -11,11 +11,12 @@ module(..., package.seeall)
 
 local modular = require("core.modular")
 local os = require("os")
+local norm_path = require("os.path").norm_path
 app = modular.inherit("gws.app")
 
 app:set_addr(nil, 8080)
 app:set_session_storage_path(os.getenv("HOME") .. "/session/")
-app:set_static_path(os.getenv("HOME") .. "/Dev/GalaLua/Test/test_webframework/statics/")
+app:set_static_path(norm_path(os.getenv("PWD") .. "../../Test/test_webframework/statics/"))
 app:set_log_path(os.getenv("HOME") .. "/log")
 app:set_urls{
     ["^/([^/]*)$"] = _PACKAGE .. "main.index",
