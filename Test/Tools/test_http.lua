0e97d7f864c31a3db670edab1fabc4f712b1dac8
wenxueliu <wenxueliu73@gmail.com>
2014-08-15 19:06:44 +0800

    test_http.lua support concurrent

--- Test/Tools/test_http.lua
+++ Test/Tools/test_http.lua
@@ -5,6 +5,8 @@
 	== Introduction ==
     test webframework with http_client
 
+    == TODO ==
+    logging
 ]]
 
 module(...,package.seeall)
@@ -12,23 +14,114 @@ local pprint = require("pprint").pprint
 local httpClient = require("http_client").httpClient
 local httpRequest = require("http_client").httpRequest
 
-function main(timer, times, mod_name, entry,...)
-    for i = 1, times do
-        timer[i] = {}
-        timer[i]["begin"] =  glr.time.now()
-        local ret, err = glr.spawn(mod_name, entry,...)
-        if err then
-            pprint(err,"err")
-        end
-        timer[i]["end"] = glr.time.now()
-        --glr.npt.registered()
+local _queue = require("collections.init").queue
+
+
+queue_server = {}
+
+function queue_server:new()
+    o = {}
+    setmetatable(o,self)
+    self.__index = self
+    return o
+end
+
+function queue_server:init()
+    self._customerq = _queue:new()
+    self._dequeue_timeout = 300
+    self._data_timeout = 30000
+    return self
+end
+
+function queue_server:get()
+    self._customerq:pop_left()
+end
+
+function queue_server:put(desc)
+    self._customerq:push_right(desc)
+end
+
+
+pool = {}
+
+function pool:new()
+    o = {}
+    setmetatable(o,self)
+    self.__index = self
+    return o
+end
+
+
+--params = { ["timeout"] = 30, ["min"] = 32, ["max"]=1024 }
+function pool:init(sup,params)
+    self._sup = sup
+
+    self._count = 0
+    self._min = params.min or 10
+    self._max = params.max or 1000
+    self._step = params.setp or 10
+
+    self._timeout = params.timeout
+
+    self.queue = queue_server:new():init()
+
+    for i = 1, self._min do
+        self:_create_process(self._sup)
+    end
+
+    return self
+end
+
+
+function pool:get_process()
+    --local p = self.queue:get()
+    print("call get_process")
+    --local p = self.queue._customerq:pop_left()
+    local p = self.queue:get()
+    print("queue object",p)
+    if not p  and self._count < self._max then
+        print("p is nil")
+        self:_create_process(self._sup)
+
+        --TODO step
+        --for i  = 1, self._step do
+        --    self:_create_process(self._sup)
+        --end
+        --p = self.queue:get()
+        p = self.queue._customerq:pop_left()
+    end
+    return p
+end
+
+function pool:_create_process(sup)
+    print("call create_process")
+    self.queue:put(sup)
+    self._count = self._count + 1
+    print("count",self._count)
+    -- TODO: error handling and logging
+end
+
+function pool:call(mod_name,entry,...)
+    local ret, err = self._sup.spawn(mod_name, entry,...)
+    if err then
+        pprint(err,"err")
     end
 end
 
-function http_connect(url)
+function pool:capacity()
+    return self._max
+end
+
+
+function http_connect(timer,cnt,url)
     local cli = httpClient:new()
-    local req = httpRequest:new():init("GET", url)
-    local res = cli:get_Response(req,20)
+    timer[cnt] = {}
+    timer[cnt]["begin"] =  glr.time.now()
+    for i = 1,1 do
+        local req = httpRequest:new():init("GET", url)
+        local res = cli:get_Response(req,20)
+    end
+    timer[cnt]["end"] = glr.time.now()
     --return res
     --glr.exit()
 end
@@ -94,10 +187,27 @@ if ... == "__main__" then
     local entry = "http_connect"
     local url = "http://127.0.0.1:8080/statics/index.html"
     local timer =  {}
-    local times = 10 
+    local cnt = 0
+    local params = {
+        ["timeout"] = 30,
+        ["min"] = 1,
+        ["step"] = 2,
+        ["max"] = 10
+    }
 
-    main(timer, times, mod_name, entry, url)
-    write_timer(timer, times)
-    tps(times)
-    --print(http_connect(url))
+    local sup = glr
+
+    local http_pool = pool:new():init(sup,params)
+
+
+    p = http_pool:get_process()
+    while p do
+        cnt = cnt + 1
+        p.spawn(mod_name,entry,timer,cnt,url)
+        p = http_pool:get_process()
+    end
+
+    local times = cnt
+    --write_timer(timer, times)
+    --tps(times)
 end
