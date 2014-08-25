da85eae2c2aa5cf55cce1e16a53d0c2b7834f3b5
Ma Tao <ma.tao@cfischina.com>
2014-08-25 16:52:42 +0800

    修改进程启动超时

--- Frameworks/core/supervisor.lua
+++ Frameworks/core/supervisor.lua
@@ -109,7 +109,7 @@ function server:start_process(params, desc)
     end
     self._logger:info("spawn:", addr, errmsg)
     -- 处理启动失败的情况
-    local mtype, desc, msg = glr.recv_by_condition(function (msg) return msg[2].addr.gpid == addr.gpid and msg[1] == glr.EXIT end, 300)
+    local mtype, desc, msg = glr.recv_by_condition(function (msg) return msg[2].addr.gpid == addr.gpid and msg[1] == glr.EXIT end, 30)
     self._logger:debug("mtype:" ..  (mtype or "nil"))
     if mtype == glr.EXIT then
         self._logger:info("Fail to start:%d, %s %s %s", mtype, pformat(desc), pformat(msg),  pformat(params))
