50f82f62b002ce0e41191ae8746661c70a453a33
zhaozineng <zhao.zn@cfischina.com>
2014-11-12 15:54:56 +0800

    重构了http/client.lua

--- Modules/http/client.lua
+++ Modules/http/client.lua
@@ -1,273 +1,398 @@
+--[[
+    A clone of python's requests.
+    
+]]
 module(...,package.seeall)
 local pprint = require("pprint").pprint
 
 local socket  = require("glr.socket")
 local strUtils = require("str_utils")
+require("tab_utils")
 
-request = {}
+support_methods={GET=true, PUT=true, POST=true,DELETE=true}
 
-function request:new(...)
-   local o = {}
-   o.version = "HTTP/1.1"
-   setmetatable(o, self)
-   self.__index = self
-   return o
+local Request = {}
+request=Request
+function Request:new(...)
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    self.init(o,...)
+    return o
 end
 
-function _parse_uri(uri)
-    --print(uri)
-    local err, host, port, path
+function Request:_parse_uri(uri)
+    local index=uri:find("^"..self._schema)
+    if index then
+        uri=uri:sub(index+1)
+    end
 
-    if uri:match("http://") then
-        err, host, port, path = uri:match("(http://)([^/:]*):?(%d*)(/?.*)")
-    else
-        host, port, path = uri:match("([^/:]*):?(%d*)(/?.*)")
-    end
-    --print(err, host, port, path)
-    if port == "" then
-        port = nil
-    end
+    local http_host, path = uri:match("^([^/]+)(.*)$")
     if path == "" then
-        path = nil
+        path = "/"
     end
-    return host,port,path
-end
+    self._http_host = http_host
+    self._path = path
 
-function request:init(type, uri)
-    assert(type=="GET" or type=="POST", "Invalid HTTP Method")
-    self._type = type
-    local host, port, path = _parse_uri(uri)
-    self._host = host
-    self._path = path or "/"
-    self._port = port or 80
-    self._params = {} -- {{k,v},{k1,v1}}
-    return self
-end
-
-function request:set_param(k,v)
-    self._params[k] = v
-end
-
-function request:to_string()
-    local lines = {}
-    lines[#lines+1] = string.format("%s %s %s\r\n", self._type, self._path, self.version)
-    self:set_param("Host",self._host)
-    self:set_param("Connection","keepalive")
-    self:set_param("Accept","*/*")
-    for k, v in pairs(self._params) do
-        lines[#lines+1] = string.format("%s:%s\r\n",k,v)
+    local index=self._http_host:find(":")
+    if index then
+        self._host=self._http_host:sub(1,index-1)
+        self._port=assert(tonumber(self._http_host:sub(index+1)),"invalid url")
+    else
+        self._host=self._http_host
+        self._port=80
     end
-    lines[#lines+1] = "\r\n"
-    --local content = {}
-    -- table.foreachi(self._params, function (idx)
-    --                                    local item = self._params[idx]
-    --                                    content[#content + 1] = string.format("%s=%s",item[1], item[2])
-    --                                end)
-    -- content = table.concat(content, "&")
-    -- print(content)
-    -- lines[#lines+1] = string.format("Content-Length:%d", content:len())
-    -- lines[#lines+1] = content
-    -- pprint.pprint(lines)
-    return table.concat(lines)
 end
 
-local get_statusCode =  function(line)
-    local s = string.match(line,"^%s*HTTP/%d%.%d%s+(%d%d%d)%s+.+$")
-    s = s or "not status line"
-    return tonumber(s)
+function Request:get_host()
+    return self._host
 end
 
-client = {}
-
-function client:new()
-   local o = {}
-   setmetatable(o, self)
-   self.__index = self
-   return o
+function Request:get_port()
+    return self._port
 end
 
-function client:init(host,port)
+function Request:get_http_host()
+    return self._http_host
+end
+
+--[[
+TODO:
+    1.Only support http right now, should support https.
+    2.根据@method设置默认header，以及增加更严格的错误检查
+]]
+function Request:init(url,method,headers,data)
+
+    self._default_method="GET"
+
+    self._schema="http://"
+    self._version = "HTTP/1.1"
+    self._user_agent="GLR/1.0"
+    self._sep="\r\n"
+
+    self:_parse_uri(url)
+
+    self._method=method or self._default_method
+
+    self._headers={
+        ["User-Agent"]=self._user_agent,
+        Host=self._http_host,
+        Connection="Keep-Alive",
+        Accept="*/*",
+    }
+
+    if self._method=="POST" then
+        self:add_headers({["Content-Type"]="application/x-www-form-urlencoded"})
+    end
+
+    self:add_headers(headers)
+
+    self:set_data(data)
+end
+
+function Request:set_data(data)
+    self._data=data or ""
+    self:add_headers({["Content-Length"]=#self._data})
+end
+
+--[[
+TODO: 1.支持多种参数模式：add_headers(key,value,key,value)和 add_headers({key=value,key=value})
+]]
+function Request:add_headers(headers)
+    if headers then
+        assert(type(headers)=="table","invalid headers")
+        table.update(self._headers,headers)
+    end
+end
+
+function Request:to_string()
+    local lines = {string.format("%s %s %s", self._method, self._path, self._version)}
+
+    for k, v in pairs(self._headers) do
+        lines[#lines+1] = string.format("%s: %s",k,v)
+    end
+
+    --\r\n after header
+    lines[#lines+1]=""
+
+    lines[#lines+1]=self._data or ""
+
+    return table.concat(lines,self._sep)
+end
+
+
+
+local HTTPConn={}
+HTTPConnection = HTTPConn
+function HTTPConn:new()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    self.init(o)
+    return o
+end
+
+function HTTPConn:init()
+
+end
+
+function HTTPConn:get_request_num()
+    return self.request_num
+end
+
+function HTTPConn:connect(host,port)
+    --self.host和self.port赋值后不可变
+    if self.host then
+        assert(host==self.host and port==self.port,string.format("Can only send request to http://%s:%d.",self.host,self.port))
+        if self._socket then
+            self.request_num=self.request_num + 1
+--            print("reuse connection",host,port)
+            return
+        end
+    else
+        self.host=assert(host,"host is nil")
+        self.port=port or 80
+    end
+
+--    print("new connection to",host,port)
     self._socket = socket.socket:new()
-    local ok,errmsg=self._socket:connect(host, port)
-    if not ok then
-        return false,errmsg
-    end
-    return ok,errmsg
+    assert(self._socket:connect(self.host, self.port))
+    self.request_num=1
 end
 
-function client:send_request(req)
-    local ret, err_msg = self._socket:send(req:to_string())
-    if not ret then
-        err_msg = string.format("client send_request error: %s",err_msg)
-        return ret,err_msg
-    end
-    return ret,err_msg
+function HTTPConn:get_statusCode(line)
+    return tonumber(string.match(line,"^%s*HTTP/%d%.%d%s+(%d%d%d)%s+.+$"))
 end
 
-function client:get_response()
-    local ret, err_msg = self:_get_response()
-    if not ret then
-        err_msg = string.format("client get_response recv error: %s",err_msg)
-    end
-    return ret,err_msg
+function HTTPConn:send_request(req)
+    self:connect(req:get_host(),req:get_port())
+
+    self:safe_assert(self._socket:send(req:to_string()))
 end
 
-function client:close()
-    self._socket:close()
+function HTTPConn:safe_assert(flag,...)
+    if not flag then
+        self:close()
+        error(string.format(...))
+    end
+    return flag
 end
 
-function client:_get_response(timeout)
-    local timeout = timeout or 6000
-    local response = {}
-    local initLine,err_msg = self._socket:recvLine(timeout)
-    if not initLine then
-        err_msg= string.format("initLine error: %s",err_msg)
-        return initLine,err_msg
+function HTTPConn:close()
+    if self._socket then
+        self._socket:close()
+        self._socket=nil
     end
+end
 
-    initLine=initLine:trim()
-    -- get header
-    response["status_code"] = get_statusCode(initLine)
-
+function HTTPConn:recv_headers(timeout)
+    local headers={}
     while true do
         local line,err_msg = self._socket:recvLine(timeout)
-        if not line then
-            err_msg = string.format("recvLine error: %s",err_msg)
-            return line,err_msg
-        end
+        self:safe_assert(line,"recvLine error: %s",err_msg)
         line=line:trim()
         if line == "" then
             break
         end
-        local key, value = unpack(string.split(line, ":"))
-        if value then
-            response[key] = value:trim()
-        else
-            response[key] = nil
-        end
+        local key, value = string.match(line, "([^:]+):(.+)")
+        self:safe_assert(key,string.format("invalid headers: %s",line))
+        headers[key] = value:trim()
     end
+    return headers
+end
 
-    -- get body
-    if response["Content-Length"] then
-        local content,err_msg =
-                self._socket:recv(tonumber(response["Content-Length"]), timeout)
-        if not content then
-            local err_msg = string.format("recv error: %s",err_msg)
-            return content, err_msg
-        end
-        response.content = content
-    elseif response["Transfer-Encoding"] == "chunked" then
-        response["content"] = ""
-        local cnt = 0
+function HTTPConn:recv_initline(timeout)
+    local initLine,err_msg = self._socket:recvLine(timeout)
+    self:safe_assert(initLine, string.format("initLine error: %s",err_msg))
+    initLine=initLine:trim()
+    return self:get_statusCode(initLine)
+end
+
+function HTTPConn:recv_body(headers,timeout)
+    local content_length= tonumber(headers["Content-Length"])
+
+    if content_length then
+        local content,err_msg = self._socket:recv(content_length, timeout)
+        return self:safe_assert(content, string.format("recv content error: %s",err_msg))
+
+    elseif headers["Transfer-Encoding"] == "chunked" then
+        local chunk={}
         while true do
             local len,errmsg = self._socket:recvLine(timeout)
-            if len:trim() ~= "0" then
-                len = "0x" .. len
-                len = tonumber(len) + 2
-                local content,errmsg = self._socket:recv(len,timeout)
-                response["content"] =
-                string.format("%s%s",response["content"],content:trim() or "")
-            else
-                len = "0x" .. len
-                len = string.format("%d",len) + 2
-                local content,errmsg = assert(self._socket:recv(len,timeout))
+            if len:trim() == "0" then
+                local content,errmsg = self._socket:recv(2,timeout)
+                self:safe_assert(content, string.format("invalid chunked message:%s",errmsg))
                 break
+            else
+                len = tonumber(len,16) + 2
+                local content,errmsg = self._socket:recv(len,timeout)
+                self:safe_assert(content,string.format("invalid chunked message:%s",errmsg))
+                chunk[#chunk+1] = content:trim()
             end
-
         end
+        return table.concat(chunk)
     end
+end
 
-    local header = {}
-    for k,v in pairs(response) do
-        if k ~= "content" then
-            header[k] = v
-        end
-    end
-    response["header"] = header
+function HTTPConn:get_response(timeout)
+    local timeout = timeout or 6000
+
+    local response = {}
+
+    response.status_code = self:recv_initline(timeout)
+
+    local headers = self:recv_headers(timeout)
+    response.headers = headers
+
+    response.content=self:recv_body(headers,timeout)
+    
     return response
 end
 
-function find_all_urls(uri, response)
-    local len = #response["content"]
-    local content = response["content"]
-    local s,e,cap = 1,1,""
-    local urls = {}
-    local i = 1
-    while e <= len do
-	s,e, cap = string.find(content,"src=(.-)>",s)
-        if cap == nil then
-            break
+
+
+local Client={}
+client=Client
+function Client:new()
+    local o={}
+    setmetatable(o,self)
+    self.__index=self
+    self.init(o)
+    return o
+end
+
+function Client:init()
+    self._http_connections={}
+    self.retry=2
+end
+
+--[[
+TODO: table参数转化为url查询参数
+如：{key1= 'value1', key2= 'value2'}===>"?key2=value2&key1=value1"
+]]
+function Client:params_encode(parameters)
+    return ""
+end
+
+--[[
+@req: string==> url字符串
+      table ==>  request对象
+]]
+function Client:get_conn(req)
+    if type(req)=="string" then
+        req=Request:new(req)
+    end
+
+    local http_host=req:get_http_host()
+
+    local http_conn=self._http_connections[http_host]
+    if http_conn==nil then
+        http_conn=HTTPConn:new()
+        self._http_connections[http_host]=http_conn
+    end
+    return http_conn
+end
+
+function Client:close_conn(url)
+    self:get_conn(url):close()
+end
+
+--[[
+@kwargs: {headers={},data=, params=}
+    @headers: 
+    @data: 
+    @params: url parameters
+    @timeout: request timeout
+]]
+function Client:request(method,url,kwargs)
+    local kwargs=kwargs or {}
+    local timeout=kwargs.timeout
+
+    local url=url..self:params_encode(kwargs.params)
+
+    local req=Request:new(url,method,kwargs.headers,kwargs.data)
+
+    local http_conn=self:get_conn(req)
+
+    --[[
+        即使客户端请求r1中使用HTTP1.1或者headers里包含"Connection: Keep-Alive"，服务器仍然可能在该请求完成后关闭连接，
+        导致下一个请求（即r2）重用该连接可能会失败，因此应该确保请求r2有一次重新建立连接的机会。
+    ]]
+    local ok,res
+    for i=1,self.retry do
+        ok,res=pcall(function()
+                http_conn:send_request(req)
+                return http_conn:get_response(timeout)
+            end)
+        if ok then
+            return res
+        elseif not ok then
+            if http_conn:get_request_num()>1 then
+                --print("reuse connection error:",res)
+            else
+                break
+            end
         end
-        urls[i] = uri .. string.sub(cap,2,#cap-1)
-        i = i + 1
-	--print("url",uri .. string.sub(cap,2,#cap-1))
-        s = e + 1
     end
-    return urls
+    error(res)
 end
 
-function client:recur_request(url,depth)
-    ---print("--------url-----------",url)
-    local uri = "http://"
-    if string.match(url,"http") then
-       uri = uri .. string.match(url,"//(.-)/")
-    end
-    uri = uri .. string.match(url,"(.-)/")
-    depth = depth + 1
-    local req = request:new():init("GET", url)
-    --local cli = client:new()
-
-    self:send_request(req);
-    local response,err_msg = self:get_response()
-    if not response then
-        err_msg = string.format("get_response error:%s", err_msg)
-        return response, err_msg
-    end
-    urls  = find_all_urls(uri,response)
-    if urls == "" then
-	    return response
-    end
-
-    pprint(urls,"-------depth:".. depth .. "-------index:".. 0 .. "---- url:".. url .."--------------")
-    for i,_url in pairs(urls) do
-        print("-------depth:".. depth .. "-------index:".. i .. "---- url:".. _url .."--------------")
-        self:recur_request(_url,depth)
-    end
+for m in pairs(support_methods) do
+    Client[string.lower(m)]= function (self,...) return self:request(m,...) end
 end
 
-function test_recur_request()
-    local uri = "127.0.0.1:8080"
-    local path = "/static/index.html"
-    url= uri .. path
-    local depth = 0
-    local cli = client:new()
-
-    if recur_request then
-        cli:recur_request(url,depth)
-    end
-end
-
-function test_http_client()
-    --local url = "www.baidu.com"
-    local url = "127.0.0.1:8080"
-
-    local req = request:new():init("GET",url)
-    local cli = client:new()
-    print("req:to_string:")
-    print(req:to_string())
-    print(req._host, req._port)
-    cli:init(req._host,req._port)
-    local res, err_msg = cli:send_request(req)
-    if not res then
-        print(res, err_msg)
-        return res,err_msg
-    end
-    res,err_msg = cli:get_response()
-    print("status_code: ",res["status_code"])
-    --pprint(res["header"],"header")
-    pprint(res,"header")
-end
 
 if ... == "__main__" then
-    test_http_client()
+    local cjson=require "cjson"
+
+    local function test_HTTPConnection(url,url2)
+        local req=Request:new(url,"GET",{["Content-Type"]="application/json"},cjson.encode{abc=122})
+
+        local http_conn=HTTPConnection:new()
+        http_conn:send_request(req)
+        pprint(http_conn:get_response())
+    
+        if url2 and url1~=url2 then
+            --应该要出错
+            local req_err=Request:new(url2,"GET",{["Content-Type"]="application/json"},cjson.encode{abc=122})
+            http_conn:send_request(req_err)
+        end
+    end
+
+    local function test_client(url,url2)
+        local cli=Client:new()
+        pprint(cli:get(url))
+        print(":::::::::::::::")
+        print("wait 65s for connection down")
+        glr.time.sleep(65)
+        pprint(cli:get(url))
+        print(":::::::::::::::")
+        if url2 then
+            pprint(cli:get(url2))
+        end
+    end
+
+    local function test_client_close(url)
+        local cli=Client:new()
+        pprint(cli:get(url))
+        print(":::::::::::::::")
+        pprint(cli:get(url))
+        print(":::::::::::::::")
+        cli:close_conn(url)
+        pprint(cli:get(url))
+        print(":::::::::::::::")
+    end
+
+
+    local url=glr.get_option("u") or "http://httpbin.org"
+
+    local url2=glr.get_option("U")
+--[[
+    test_HTTPConnection(url,url2)
+
+    test_client(url)
+
+    ]]
+    test_client_close(url)
+
 end
